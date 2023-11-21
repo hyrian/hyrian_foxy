1978e609fda069f7a24b2e8585042caf973b9dea
John-woohyeon <facility1@hanyang.ac.kr>
2023-11-21 05:40:19 +0000

    FEAT : Merged file modified

---
+++ src/hyrian_foxy/hyrian_bringup/launch/hyrian_bringup.launch.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+# Author: John-woohyeon 
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import ThisLaunchFileDir
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+  hyrian_mcu_parameter = LaunchConfiguration(
+    'hyrian_mcu_parameter',
+    default=os.path.join(
+      get_package_share_directory('hyrian_bringup'),
+      'param/hyrian_mcu.yaml'
+    )
+  )
+
+  hyrian_lidar_parameter = LaunchConfiguration(
+    'hyrian_lidar_parameter',
+    default=os.path.join(
+      get_package_share_directory('hyrian_bringup'),
+      'param/hyrian_lidar.yaml'
+    )
+  )
+
+  use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+  hyrian_description_dir = LaunchConfiguration(
+    'hyrian_description_dir',
+    default=os.path.join(
+      get_package_share_directory('hyrian_description'),
+      'launch'
+    )
+  )
+
+  return LaunchDescription([
+    DeclareLaunchArgument(
+      'hyrian_mcu_parameter',
+      default_value=hyrian_mcu_parameter
+    ),
+
+    DeclareLaunchArgument(
+      'hyrian_lidar_parameter',
+      default_value=hyrian_lidar_parameter
+    ),
+
+    IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/hyrian_mcu.launch.py']),
+      launch_arguments={'hyrian_mcu_parameter': hyrian_mcu_parameter}.items()
+    ),
+    
+    IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/hyrian_lidar.launch.py']),
+      launch_arguments={'hyrian_lidar_parameter': hyrian_lidar_parameter}.items()
+    ),
+    
+    IncludeLaunchDescription(
+      PythonLaunchDescriptionSource([hyrian_description_dir, '/hyrian_state_publisher.launch.py']),
+      launch_arguments={'use_sim_time': use_sim_time}.items(),
+    ),
+    # Node(
+    #         package='rudolph_params',
+    #         executable='publish_params.py',
+    #         name='rudolph_params',
+    #         output='screen'
+    #     ),
+    #     # Launch wheel encoder tick publisher node
+    # Node(
+    #     package='hyrian_motor',
+    #     executable='encoder_publisher.py',
+    #     name='hyrian_motor',
+    #     output='screen'
+    # ),
+    # Node(
+    #     package='hyrian_motor',
+    #     executable='odometer_publisher.py',
+    #     name='hyrian_odom',
+    #     output='screen'
+    # ),
+  ])
