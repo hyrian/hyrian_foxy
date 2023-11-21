a2fcc7a893b0bd0f2cf5c7bbadc694b3f9b0b518
John-woohyeon <facility1@hanyang.ac.kr>
2023-11-21 17:03:16 +0000

    FEAT : laser_frame fixed

--- hyrian_description/launch/hyrian_state_publisher.launch.py
+++ hyrian_description/launch/hyrian_state_publisher.launch.py
@@ -18,7 +18,9 @@ def generate_launch_description():
 
     share_dir = get_package_share_directory('hyrian_description')
     xacro_file = os.path.join(share_dir, 'urdf', 'hyrian.xacro')
+    # robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
 
+    # params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
     # urdf_file_name = 'omo_r1mini.urdf'
 
     # urdf = os.path.join(
@@ -26,12 +28,24 @@ def generate_launch_description():
     #     'urdf',   
     #     urdf_file_name)
 
+    # node_robot_state_publisher = Node(
+    #     package='robot_state_publisher',
+    #     executable='robot_state_publisher',
+    #     output='screen',
+    #     parameters=[params]
+    # )
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
             description='Use simulation (Gazebo) clock if true'),
 
+        # DeclareLaunchArgument(
+        #     'use_ros2_control',
+        #     default_value='true',
+        #     description='Use ros2_control if true'),
+
+        # node_robot_state_publisher
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
