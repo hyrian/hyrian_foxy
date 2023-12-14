c5129b747d7fb39e9430c156f23b01f0ea118378
seungmin son <sonsean89@gmail.com>
2023-12-14 09:28:42 +0900

    FEAT : BT_Baseline

---
+++ hyrian_navigation2/src/bt_main.cpp
@@ -0,0 +1,15 @@
+// #include "nav2_client.hpp"
+#ifdef SUPPORT_OPENVINO
+#endif
+#include <behaviortree_cpp_v3/bt_factory.h>
+#include <behaviortree_cpp_v3/loggers/bt_cout_logger.h>
+
+// #define DEFAULT_BT_XML "/home/ros/bt_ros2_ws/src/BT_ros2/bt_xml/bt_nav_mememan.xml"
+
+using namespace BT;
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
