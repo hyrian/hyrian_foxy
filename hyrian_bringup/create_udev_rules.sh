b68e8b00d226371a7be4f523ec5cec5983099d56
John-woohyeon <facility1@hanyang.ac.kr>
2023-12-16 21:20:14 +0000

    FEAT : Fix Port

--- hyrian_bringup/create_udev_rules.sh
+++ hyrian_bringup/create_udev_rules.sh
@@ -4,13 +4,13 @@ echo ""
 echo "This script copies Hyrian udev rules to /etc/udev/rules.d/"
 echo ""
 
-echo "Motor Driver (USB Serial from RS232) : /dev/ttyTHS1 to /dev/ttyMCU :"
-if [ -f "/etc/udev/rules.d/98-hyrian-mcu" ]; then
-    echo "98-hyrian-mcu file already exist."
-else 
-    echo 'KERNEL=="ttyTHS1", MODE:="0666", GROUP:="dialout", SYMLINK+="ttyMCU"' > /etc/udev/rules.d/98-hyrian-mcu.rules   
-    echo '98-hyrian-mcu created'
-fi
+# echo "Motor Driver (USB Serial from RS232) : /dev/ttyTHS1 to /dev/ttyMCU :"
+# if [ -f "/etc/udev/rules.d/98-hyrian-mcu" ]; then
+#     echo "98-hyrian-mcu file already exist."
+# else 
+#     echo 'KERNEL=="ttyTHS1", MODE:="0666", GROUP:="dialout", SYMLINK+="ttyMCU"' > /etc/udev/rules.d/98-hyrian-mcu.rules   
+#     echo '98-hyrian-mcu created'
+# fi
 
 echo ""
 echo "YD LiDAR (USB Serial) : /dev/ttyUSBx to /dev/ttyLiDAR :"
@@ -29,7 +29,7 @@ if [ -f "/etc/udev/rules.d/96-hyrian-imu.rules" ]; then
     echo "hyrian-imu.rules file already exist."
 else 
     # echo 'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4" ATTRS{idProduct}=="ea60", MODE:="0666", GROUP:="dialout", SYMLINK+="ttyIMU" ' > /etc/udev/rules.d/96-hyrian-imu.rules
-    echo 'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", ATTRS{serial}=="b4f8aa510b9eec119c81738a43a0c072", MODE:="0666", GROUP:="dialout", SYMLINK+="ttyIMU"' > /etc/udev/rules.d/96-hyrian-imu.rules
+    echo 'SUBSYSTEM=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", ATTRS{serial}=="b4f8aa510b9eec119c81738a43a0c072", MODE:="0666", GROUP:="dialout", SYMLINK+="ttyIMU"' > /etc/udev/rules.d/96-hyrian-imu.rules
 
     echo '96-hyrian_imu.rules created'
 fi
