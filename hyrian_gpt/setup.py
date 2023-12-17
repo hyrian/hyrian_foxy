eab779681a3b9ed333903f57c4a8099eda1da0b1
John-woohyeon <jungwoohyeon@gmail.com>
2023-12-18 08:00:59 +0900

    REFACTORY : Refactoried Package Upload - build type fixed

--- hyrian_gpt/setup.py
+++ hyrian_gpt/setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-package_name = 'gpt_foxy'
+package_name = 'hyrian_gpt'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.1.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,14 +13,15 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Your Name',
-    maintainer_email='your-email@example.com',
-    description='A ROS 2 package for GPT interaction.',
-    license='Apache License 2.0',
+    maintainer='john',
+    maintainer_email='jungwoohyeon@gmail.com',
+    description='A ROS 2 package for GPT interaction',
+    license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'conversation_pub = gpt_foxy.conversation_pub:main'
+            'gpt_pub_test = gpt_test.gpt_pub_test:main',
+            'keyword_sub = gpt_test.keyword_sub:main',
         ],
     },
 )
